e056ef9b21394913239d2053fe566688a8c951fc
farzadafi <farzadafi40@gmail.com>
2022-08-10 16:51:31 +0430

    add some field for user

--- src/main/java/model/dto/UserDto.java
+++ src/main/java/model/dto/UserDto.java
@@ -1,4 +1,14 @@
 package model.dto;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Builder
 public class UserDto {
+    private String firstName;
+    private String lastName;
+    private String userName;
 }
