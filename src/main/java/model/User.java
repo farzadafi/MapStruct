525bded4064b10225d03091f49293687b9423e30
farzadafi <farzadafi40@gmail.com>
2022-08-10 16:50:11 +0430

    add some field for user

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,14 @@
 package model;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Builder
 public class User {
+    private String firstName;
+    private String lastName;
+    private String userName;
 }
