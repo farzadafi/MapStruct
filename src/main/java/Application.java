81790dab7c76044bfcca3e7cfeb09b7d87091830
farzadafi <farzadafi40@gmail.com>
2022-08-11 06:00:44 +0430

    write test for simple map between Dto to model

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,12 @@
+import dto.UserDto;
+import mapper.interfaces.UserMapper;
+import model.User;
+
 public class Application {
+    public static void main(String[] args) {
+        UserDto userDto = new UserDto("farzad","afshar","farzadAfi");
+        User user = UserMapper.INSTANCE.dtoToModel(userDto);
+        System.out.println("userDto is " + userDto);
+        System.out.println("user is(map)" + user);
+    }
 }
