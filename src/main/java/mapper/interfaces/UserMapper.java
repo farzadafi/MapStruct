e0eb6c3788f950cad3083d24e9dc80de64715c62
farzadafi <farzadafi40@gmail.com>
2022-08-11 06:07:27 +0430

    add dtoToModelWithChangeLFName method

--- src/main/java/mapper/interfaces/UserMapper.java
+++ src/main/java/mapper/interfaces/UserMapper.java
@@ -3,6 +3,7 @@ package mapper.interfaces;
 import dto.UserDto;
 import model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -11,4 +12,8 @@ public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     User dtoToModel(UserDto userDto);
+
+    @Mapping(target="lastName", source="firstName")
+    @Mapping(target="firstName", source="lastName")
+    User dtoToModelWithChangeLFName(UserDto userDto);
 }
